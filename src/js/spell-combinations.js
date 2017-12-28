df4d4194bd450ba7ad2ba74cd1c656a30b8fabeb
Kamyil <plkamil1998@gmail.com>
2017-12-28 13:39:06 +0100

    Importing project to GitHub

---
+++ src/js/spell-combinations.js
@@ -0,0 +1,27 @@
+	/*
+	Invoker's spell combinations
+
+	1.QQQ - Cold Snap
+	2.QQW - Ghost Walk
+	3.QWW - Tornado
+	4.QQE - Ice Wall
+	5.QEE - Forge Spirit
+	6.QWE - Deafening Blast
+	7.QEW - Deafening Blast
+	
+	1.WWW - E.M.P
+	2.WWQ - Tornado
+	3.WQQ - Ghost Walk
+	4.WWE - Alacrity
+	5.WEE - Chaos Meteor
+	6.WQE - Deafening Blast
+	7.WEQ - Deafening Blast
+
+	1.EEE - Sun Strike
+	2.EEQ - Forge Spirit
+	3.EQQ - Ice Wall 
+	4.EEW - Chaos Meteor
+	5.EWW - Alacrity
+	6.EQW - Deafening Blast
+	7.EWQ - Deafening Blast
+	*/
---
+++ src/js/spell-taken.js
@@ -0,0 +1,155 @@
+	
+	
+	let container = document.getElementById("elements-taken");
+	let spell_container = document.getElementById("spell-invoked");
+	let update = [];
+	let spell; 
+	
+	
+	
+	document.addEventListener("keypress", function (e) {
+
+		let element = document.createElement('div');
+		//A - Quas
+		if (e.keyCode == 97) {
+
+
+			element.className = "quas";
+			update.push('quas');
+			container.appendChild(element);
+		}
+		//S - Wex
+		if (e.keyCode == 115) {
+
+			element.className = "wex";
+			update.push('wex');
+			container.appendChild(element);
+		}
+		//D - Exort
+		if (e.keyCode == 100) {
+
+			element.className = "exort";
+			update.push('exort');
+			container.appendChild(element);
+
+		}
+
+		
+
+
+
+
+
+
+
+		//F - Invoke
+		if (e.keyCode == 102) {
+			console.log(update);
+			//BEGINNING WITH QUAS
+			if(update=="quas,quas,quas"){
+				spell="Cold Snap";
+				console.log(spell);
+			}
+			else if(update=="quas,quas,wex"){
+				spell="Ghost Walk";
+				console.log(spell);
+			}
+			else if(update=="quas,wex,wex"){
+				spell="Tornado";
+				console.log(spell);
+			}
+			else if(update=="quas,quas,exort"){
+				spell="Ice Wall";
+				console.log(spell);
+			}
+			else if(update=="quas,exort,exort"){
+				spell="Forge Spirit";
+				console.log(spell);
+			}
+			else if(update=="quas,wex,exort"){
+				spell="Deafening Blast";
+				console.log(spell);
+			}
+			else if(update=="quas,exort,wex"){
+				spell="Deafening Blast";
+				console.log(spell);
+			}
+
+
+			//BEGINNING WITH WEX
+			else if(update=="wex,wex,wex"){
+				spell="E.M.P";
+				console.log(spell);
+			}
+			else if(update=="wex,wex,quas"){
+				spell="Tornado";
+				console.log(spell);
+			}
+			else if(update=="wex,quas,quas"){
+				spell="Ghost Walk";
+				console.log(spell);
+			}
+			else if(update=="wex,wex,exort"){
+				spell="Alacrity";
+				console.log(spell);
+			}
+			else if(update=="wex,exort,exort"){
+				spell="Chaos Meteor";
+				console.log(spell);
+			}
+			else if(update=="wex,quas,exort"){
+				spell="Deafening Blast";
+				console.log(spell);
+			}
+			else if(update=="wex,exort,quas"){
+				spell="Deafening Blast";
+				console.log(spell);
+			}
+
+			//BEGGINING WITH EXORT
+
+			else if(update=="exort,exort,exort"){
+				spell="Sun Strike";
+				console.log(spell);
+			}
+			else if(update=="exort,exort,quas"){
+				spell="Forge Spirit";
+				console.log(spell);
+			}
+			else if(update=="exort,quas,quas"){
+				spell="Ice Wall";
+				console.log(spell);
+			}
+			else if(update=="exort,exort,wex"){
+				spell="Chaos Meteor";
+				console.log(spell);
+			}
+			else if(update=="exort,wex,wex"){
+				spell="Alacrity";
+				console.log(spell);
+			}
+			else if(update=="exort,quas,wex"){
+				spell="Deafening Blast";
+				console.log(spell);
+			}
+			else if(update=="exort,wex,quas"){
+				spell="Deafening Blast";
+				console.log(spell);
+			}
+
+		}
+
+		
+
+		
+		if (update.length == 4) {
+			update.shift();
+			container.removeChild(container.childNodes[0,1]);
+
+		}
+		
+
+
+	}, false);
+
+
