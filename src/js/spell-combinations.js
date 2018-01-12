e86aee4a5228f40d5d4d536c2139411bef31fa5a
Kamyil <plkamil1998@gmail.com>
2018-01-12 09:42:30 +0100

    Fixed spell-combinations

--- src/js/spell-combinations.js
+++ src/js/spell-combinations.js
@@ -6,22 +6,28 @@
 	3.QWW - Tornado
 	4.QQE - Ice Wall
 	5.QEE - Forge Spirit
-	6.QWE - Deafening Blast
-	7.QEW - Deafening Blast
+	6.QWQ - Ghost Walk
+	7.QEQ - Ice Wall
+	8.QWE - Deafening Blast
+	9.QEW - Deafening Blast
 	
 	1.WWW - E.M.P
 	2.WWQ - Tornado
 	3.WQQ - Ghost Walk
 	4.WWE - Alacrity
 	5.WEE - Chaos Meteor
-	6.WQE - Deafening Blast
-	7.WEQ - Deafening Blast
+	6.WEW - Alacrity
+	7.WQW - Tornado
+	8.WQE - Deafening Blast
+	9.WEQ - Deafening Blast
 
 	1.EEE - Sun Strike
 	2.EEQ - Forge Spirit
 	3.EQQ - Ice Wall 
 	4.EEW - Chaos Meteor
 	5.EWW - Alacrity
-	6.EQW - Deafening Blast
-	7.EWQ - Deafening Blast
+	6.EWE - Chaos Meteor
+	7.EQE - Forge Spirit
+	8.EQW - Deafening Blast
+	9.EWQ - Deafening Blast
 	*/
