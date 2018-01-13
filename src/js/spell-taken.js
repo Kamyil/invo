7fb3c234f51ea87beeca05f317521247f2e468c8
Kamyil <plkamil1998@gmail.com>
2018-01-13 15:21:45 +0100

    Daily update

--- src/js/spell-taken.js
+++ src/js/spell-taken.js
@@ -5,6 +5,8 @@
 	let update = [];
 	let spell; 
 	let empty = document.querySelector('.empty');
+
+	
 	
 	
 	
@@ -52,38 +54,48 @@
 				spell="Cold Snap";
 				console.log(spell);
 				
+				empty.classList.replace('empty','cold-snap');
+				console.log(empty);
 			}
 			else if(update=="quas,quas,wex"){
 				spell="Ghost Walk";
 				console.log(spell);
+				empty.classList.replace('empty','ghost-walk');
 			}
 			else if(update=="quas,wex,wex"){
 				spell="Tornado";
 				console.log(spell);
+				empty.classList.replace('empty','tornado');
 			}
 			else if(update=="quas,quas,exort"){
 				spell="Ice Wall";
 				console.log(spell);
+				empty.classList.replace('empty','ice-wall');
 			}
 			else if(update=="quas,exort,exort"){
 				spell="Forge Spirit";
 				console.log(spell);
+				empty.classList.replace('empty','forge-spirit');
 			}
 			else if(update=="quas,wex,exort"){
 				spell="Deafening Blast";
 				console.log(spell);
+				empty.classList.replace('empty','def-blast');
 			}
 			else if(update=="quas,exort,wex"){
 				spell="Deafening Blast";
 				console.log(spell);
+				empty.classList.replace('empty','def-blast');
 			}
 			else if(update=="quas,exort,quas"){
 				spell="Ice Wall";
 				console.log(spell);
+				empty.classList.replace('empty','ice-wall');
 			}
 			else if(update=="quas,wex,quas"){
 				spell="Ghost Walk";
 				console.log(spell);
+				empty.classList.replace('empty','ghost-walk');
 			}
 
 
@@ -91,38 +103,47 @@
 			else if(update=="wex,wex,wex"){
 				spell="E.M.P";
 				console.log(spell);
+				empty.classList.replace('empty','emp');
 			}
 			else if(update=="wex,wex,quas"){
 				spell="Tornado";
 				console.log(spell);
+				empty.classList.replace('empty','tornado');
 			}
 			else if(update=="wex,quas,quas"){
 				spell="Ghost Walk";
 				console.log(spell);
+				empty.classList.replace('empty','ghost-walk');
 			}
 			else if(update=="wex,wex,exort"){
 				spell="Alacrity";
 				console.log(spell);
+				empty.classList.replace('empty','alacrity');
 			}
 			else if(update=="wex,exort,exort"){
 				spell="Chaos Meteor";
 				console.log(spell);
+				empty.classList.replace('empty','chaos-meteor');
 			}
 			else if(update=="wex,quas,exort"){
 				spell="Deafening Blast";
 				console.log(spell);
+				empty.classList.replace('empty','def-blast');
 			}
 			else if(update=="wex,exort,quas"){
 				spell="Deafening Blast";
 				console.log(spell);
+				empty.classList.replace('empty','def-blast');
 			}
 			else if(update=="wex,exort,wex"){
 				spell="Alacrity";
 				console.log(spell);
+				empty.classList.replace('empty','alacrity');
 			}
 			else if(update=="wex,quas,wex"){
 				spell="Tornado";
 				console.log(spell);
+				empty.classList.replace('empty','tornado');
 			}
 
 			//BEGGINING WITH EXORT
@@ -130,38 +151,47 @@
 			else if(update=="exort,exort,exort"){
 				spell="Sun Strike";
 				console.log(spell);
+				empty.classList.replace('empty','sun-strike');
 			}
 			else if(update=="exort,exort,quas"){
 				spell="Forge Spirit";
 				console.log(spell);
+				empty.classList.replace('empty','forge-spirit');
 			}
 			else if(update=="exort,quas,quas"){
 				spell="Ice Wall";
 				console.log(spell);
+				empty.classList.replace('empty','ice-wall');
 			}
 			else if(update=="exort,exort,wex"){
 				spell="Chaos Meteor";
 				console.log(spell);
+				empty.classList.replace('empty','chaos-meteor');
 			}
 			else if(update=="exort,wex,wex"){
 				spell="Alacrity";
 				console.log(spell);
+				empty.classList.replace('empty','alacrity');
 			}
 			else if(update=="exort,quas,wex"){
 				spell="Deafening Blast";
 				console.log(spell);
+				empty.classList.replace('empty','def-blast');
 			}
 			else if(update=="exort,wex,quas"){
 				spell="Deafening Blast";
 				console.log(spell);
+				empty.classList.replace('empty','def-blast');
 			}
 			else if(update=="exort,wex,exort"){
 				spell="Chaos Meteor";
 				console.log(spell);
+				empty.classList.replace('empty','chaos-meteor');
 			}
 			else if(update=="exort,quas,exort"){
 				spell="Forge Spirit";
 				console.log(spell);
+				empty.classList.replace('empty','forge-spirit');
 			}
 
 		}
